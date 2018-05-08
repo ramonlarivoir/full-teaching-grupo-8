e83118b43cd14aa7fc361a82c087761ddf40197d
ramonlarivoir <rlarivoir@hotmail.com>
2018-05-08 17:36:26 -0300

    Criando projeto

---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ *
+ * @author Ramon Larivoir
+ */
+public class LoginTest {
+    
+    static WebDriver driver;
+    
+    @BeforeClass
+    public static void configura() {
+        System.setProperty("webdriver.chrome.driver", "C:/Users/Ramon Larivoir/Desktop/Ramon/Programas/Selenium/chromedriver.exe");
+        driver = (WebDriver) new ChromeDriver();
+        
+        
+        driver.get("https://atlantis.isti.cnr.it:5000/");
+        
+    }
+    
+    @Test
+    public void testaLogin(){
+        WebElement nome = driver.findElement(By.tagName("Log in"));
+        nome.click();
+        
+    }
+    
+    
+}
