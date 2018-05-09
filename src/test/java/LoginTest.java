94eb66d305b93b8dffbb5b9c6e0b9e88dd118879
douglasBaumgratz <douglas.baumgratz@gmail.com>
2018-05-08 22:00:47 -0300

    addCourse e gotoSetings(!funcionando)

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -20,19 +20,18 @@ import org.openqa.selenium.chrome.ChromeDriver;
  * @author Ramon Larivoir
  */
 public class LoginTest {
-    
+
     static WebDriver driver;
-    
+
     @BeforeClass
     public static void configura() {
-        System.setProperty("webdriver.chrome.driver", "C:/Users/Ramon Larivoir/Desktop/Ramon/Programas/Selenium/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "/home/douglas/Downloads/chromedriver");
         driver = (WebDriver) new ChromeDriver();
-        
-        
+
         driver.get("https://atlantis.isti.cnr.it:5000/");
-        
+
     }
-    
+
     @Test
     public void testaLogin(){
         WebElement login = driver.findElement(By.id("download-button"));
@@ -43,6 +42,32 @@ public class LoginTest {
         senha.sendKeys("pass");
         email.submit();
     }
-    
-    
+    @Test
+    public void addCourse() {
+        WebElement login = driver.findElement(By.id("download-button"));
+        login.click();
+        WebElement email = driver.findElement(By.name("email"));
+        email.sendKeys("teacher@gmail.com");
+        WebElement senha = driver.findElement(By.name("password"));
+        senha.sendKeys("pass");
+        email.submit();
+        WebElement addcourse = driver.findElement(By.id("submit-post-course"));
+        addcourse.click();
+        WebElement nome = driver.findElement(By.name("input-post-course-name"));
+        nome.sendKeys("Computação");
+        nome.submit();
+    }
+    @Test
+    public void goToSettings() {
+        WebElement login = driver.findElement(By.id("download-button"));
+        login.click();
+        WebElement email = driver.findElement(By.name("email"));
+        email.sendKeys("nickzation@gmail.com");
+        WebElement senha = driver.findElement(By.name("password"));
+        senha.sendKeys("Mm99454391");
+        email.submit();
+        WebElement configuracoes = driver.findElement(By.id("settings-button"));
+        configuracoes.click();
+    }
+
 }
