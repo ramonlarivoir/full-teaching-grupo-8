ac916f8b7b4a0f887b59879f8faf9fecc166ec30
Igor Mageste <igor.mageste@gmail.com>
2018-06-06 21:31:43 -0300

    testes do forum

--- src/test/java/GerenciarForum/Publicacao.java
+++ src/test/java/GerenciarForum/Publicacao.java
@@ -31,7 +31,7 @@ public class Publicacao {
         WebElement login = driver.findElement(By.id("download-button"));
         login.click();
         WebElement email = driver.findElement(By.name("email"));
-        email.sendKeys("student1@gmail.com");
+        email.sendKeys("teacher@gmail.com");
         WebElement senha = driver.findElement(By.name("password"));
         senha.sendKeys("pass");
         email.submit();
@@ -45,6 +45,31 @@ public class Publicacao {
     @Test
     public void testCreate() {
         loga();
+
+        WebElement badge = driver.findElement(By.cssSelector("span.cal-day-badge"));
+        badge.click();
+
+        WebElement forward = driver.findElement(By.cssSelector("i.material-icons.calendar-event-icon"));
+        forward.click();
+
+        WebDriverWait wait = new WebDriverWait(driver, 30);
+        wait.until(visibilityOfElementLocated(By.id("forum-tab-icon")));
+
+        WebElement forum = driver.findElement(By.cssSelector("i#forum-tab-icon.material-icons.tooltipped"));
+        forum.click();
+
+        WebElement newEntry = driver.findElement(By.cssSelector("i#add-entry-icon.material-icons.forum-icon"));
+        newEntry.click();
+
+        WebElement title = driver.findElement(By.cssSelector("input#input-post-title.validate.ng-pristine.ng-invalid.ng-touched"));
+        title.sendKeys("New entry");
+
+        WebElement comment = driver.findElement(By.cssSelector("textarea#input-post-comment.materialize-textarea.validate.ng-pristine.ng-invalid.ng-touched"));
+        comment.sendKeys("Comment on entry");
+
+        WebElement send = driver.findElement(By.cssSelector("button#post-modal-btn.waves-effect.btn-flat.modal-footer-button"));
+        send.click();
+
         driver.close();
     }
 }
